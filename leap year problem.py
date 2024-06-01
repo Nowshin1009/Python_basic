83c1b466d146f0e4c72f5bf12207dbb2de02496a
nowshin1009 <nowshin35-1009@diu.edu.bd>
2024-06-01 17:31:30 +0600

    leap year with conditional

--- leap year problem.py
+++ leap year problem.py
@@ -11,3 +11,20 @@ if year%4==0:
         print(year, "is leap year")
 else:
     print(year, "is not leap year")
+
+#-------------------------------------------------------------
+# Using conditional operators:
+#-------------------------------------------------------------
+
+year= input("Enter a year")
+year=int(year)
+
+if year % 4 == 0:
+    if (year % 100 ==0 and year % 400 == 0):
+        print(year, "is a leap year")
+    elif year%100!=0 :
+        print(year,"is leap year")
+    else :
+        print(year, "is not a leap year")
+else :
+     print(year, "is not a leap year")
