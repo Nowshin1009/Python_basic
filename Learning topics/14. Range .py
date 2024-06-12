d06cad28340d9dcb15c5a0ce2acd01c2c2ed8efb
nowshin1009 <nowshin35-1009@diu.edu.bd>
2024-06-12 09:47:48 +0600

    range

--- Learning topics/14. Range .py
+++ Learning topics/14. Range .py
@@ -1,3 +1,14 @@
+# We can declare a range in 3 ways:
+li1=list(range(10))
+li2=list(range(1,21))
+li3=list(range(1,11,2))
+
+print(li1)
+print(li2)
+print(li3)
+
+
+
 range1= range(1,11)
 for x in range1:
     print(x)
@@ -12,5 +23,9 @@ for x in li:
 
 even_numbers = list(range(2,101,2))
 print(even_numbers)
+
+# Inverse way traverse:
+numbers= list(range(10,0,-1))
+print(numbers)
 
 
