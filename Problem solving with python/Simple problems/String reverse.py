0796806264345c77ffe680e5f700ce235faea660
nowshin1009 <nowshin35-1009@diu.edu.bd>
2024-07-10 22:48:49 +0600

    Exception hanlding

---
+++ Problem solving with python/Simple problems/String reverse.py
@@ -0,0 +1,9 @@
+#write a funstion that takes a list of string and returns a new list with each string reversed
+
+
+def reverse_string(strings):
+    reversed_number = [s[::-1] for s in strings]
+
+
+str_list = ["Hello", "Python", "Django"]
+print(reverse_string)
